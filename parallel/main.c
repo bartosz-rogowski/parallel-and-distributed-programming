a54c349e81f8988560111c6bd220d26290d1815a
Aleksandra Rolka <8rolka@taurus.fis.agh.edu.pl>
2022-05-05 02:25:03 +0200

    troche good troche not good

--- parallel/main.c
+++ parallel/main.c
@@ -3,19 +3,21 @@
  * Program for Prim's Minimum Spanning Tree (MST) algorithm. 
  * The program is for adjacency matrix representation of the graph.
  * ------------------------------------------------------------------------------------
- * Compile: gcc main.c -o main 
- * Run:     mpicc -n proc ./main filename
- *          where: 
- *                  -proc - number of processes
- *                  -filename - name of the file which contains input adjency matrix
- *          for example date: ./main data01.txt
+
+ *  Configure:          source /opt/nfs/config/source_mpich32.sh
+ *  Compile:            mpicc main.c -o main 
+ *  Claster setup:      /opt/nfs/config/station206_name_list.sh 1 16 > nodes
+ *  Run:                mpiexec -f nodes -n 16 ./main filename
+ *                          -proc - number of processes
+ *                          -filename - name of the file which contains input adjency matrix
+ *                      for example date: mpiexec -f nodes -n 16 ./main data01.txt
  * ------------------------------------------------------------------------------------
  */
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
 #include <string.h>
-#include <mpi.h>
+#include "mpi.h"
 
 #define MASTER 0            // Master node number
 int N;                      // number of vertices
@@ -27,13 +29,13 @@ int main(int argc, char *argv[])
     char* fileName = argv[1];
     FILE* file = fopen(fileName, "r");
     int** graph;            // adjency matrix of graph
-    int i, j, v;
+    int i, j, v, k;
 
     int rank;               // rank of current processor
     int procNum;            // number of processors
-    int* chunkSize;         // specifying the number of elements to send to each processor     // chunkSize
+    int* chunkSize;         // specifying the number of elements to send to each processor
                             // liczba kolumn
-    int* displ;             // specifies the displacement (starting index) needed in MPI_Scatterv
+    int* displs;            // specifies the displacement (starting index) needed in MPI_Scatterv
     int* chunk;             // chunk of matrix (columns) that belongs to each processor
                             // wektor elementów należących do danego procesu (rozplaszczona macierz kolumn które ma przetworzyc dany proces)
     int* MST;               // minimum spanning tree with selected edges: MST[v1] = v2
@@ -50,33 +52,18 @@ int main(int argc, char *argv[])
         int v2; 
     } edge;                   
 
-    int minWeight, v1, v2;    // for fiding localRow.minWeight
-    
+    int globalMinWeight, minWeight, min, v1, v2; 
 
 
-    MPI_Comm world = MPI_COMM_WORLD;
     MPI_Init(&argc, &argv);
+    MPI_Comm world = MPI_COMM_WORLD;
     MPI_Comm_rank(world, &rank);
     MPI_Comm_size(world, &procNum);
-    
-    chunkSize = (int*)malloc(sizeof(int) * procNum);
-    displs = (int*)malloc(sizeof(int) * procNum);
-
-    int remains = procNum - (N % procNum);  // if number of vertices is not a multiply of number of processors
-    chunkSize[0] = N / procNum;
-    displs[0] = 0;
-
-    for (i = 1; i < procNum; i++) {
-        chunkSize[i] = N / procNum;
-        if (i < remains) 
-            ++chunkSize[i];
-        displs[i] = displs[i-1] + chunkSize[i-1];
-    }
 
 
     // master processor read adjency matrix data from file to 2D matrix (graph)
     if (rank == MASTER) {
-        printf("rank is %d and number of processors is %d\n", rank, procNum);
+        printf("[INFO] Master rank is %d and number of processors is %d\n", rank, procNum);
 
         // Preparing command for counting number of lines
         char* grep = "grep \"\" -c ";
@@ -88,46 +75,112 @@ int main(int argc, char *argv[])
         if (file == NULL)
             printf("Could not open this file. Choose again proper filename..\n");
         else {
+            // Reading number of lines from Linux command
+            FILE* res = popen(bashCommand, "r");
+            if (res)
+                fscanf(res, "%d", (int*)(&N));
 
-        // Reading number of lines from Linux command
-        FILE* res = popen(bashCommand, "r");
-        if (res)
-            fscanf(res, "%d", (int*)(&N));
+            free(bashCommand);
 
-        free(bashCommand);
-
-        graph = calloc(N, sizeof(int *));
-        for(i=0; i < N; i++)    
-            graph[i] = calloc(N, sizeof(int));
+            graph = calloc(N, sizeof(int*));
+            for(i=0; i < N; i++)    
+                graph[i] = calloc(N, sizeof(int));
 
 
-        for (i = 0; i < N; i++) {
-            for (j = 0; j < N; j++) {
-                fscanf(file, "%d", &v);
-                graph[i][j] = v;
+            for (i = 0; i < N; i++) {
+                for (j = 0; j < N; j++) {
+                    fscanf(file, "%d", &v);
+                    graph[i][j] = v;
+                }
             }
-        }
-        fclose(file);
+            fclose(file);
+
+            if (procNum > N) {
+                procNum = N;
+                printf("[INFO] Proc number > graph size.. reducing to %d\n", procNum);
+            }
+
+        } // end of else
     } // end of master node tasks
 
+    MPI_Bcast(&N, 1, MPI_INT, MASTER, world);
+    
+    if(rank != MASTER)
+    {
+        graph = calloc(N, sizeof(int*));
+        for(i=0; i < N; i++)    
+            graph[i] = calloc(N, sizeof(int));
+    }
 
-    chunk = (int*)malloc(chunkSize[rank]*N*sizeof(int)); // each processor needs its own chunk of data
+    MPI_Bcast(&procNum, 1, MPI_INT, MASTER, world);
+    for (i = 0; i < N; i++) 
+        MPI_Bcast((int **)&(graph[i][0]), N, MPI_INT, MASTER, MPI_COMM_WORLD);
+    
 
-    MPI_Datatype vectorType; 
-    MPI_Type_contiguous(N, MPI_INT, &vectorType);
-    MPI_Type_commit(&vectorType);
+    
+    chunkSize = calloc(procNum, sizeof(int));
+    displs = calloc(procNum, sizeof(int));
+
+    int remains = (N % procNum);  // if number of vertices is not a multiply of number of processors
+    displs[0] = 0;
+    
+    for (i = 0; i < procNum; i++) {
+        chunkSize[i] = N / procNum;
+        if (i < remains) 
+            ++chunkSize[i];
+    }
+
+
+    for (i = 1; i < procNum; i++) {
+        displs[i] = displs[i-1] + chunkSize[i-1];
+    }
+    
+    chunk = calloc(chunkSize[rank]*N, sizeof(int));     // each processor needs its own chunk of data
+    
+
+    // for (i = 0; i < procNum; ++i) {
+    //     chunkSize[i] *= N;
+    //     displs[i] *= N;
+    // }
+
+    // MPI_Datatype vectorType; 
+    // MPI_Type_contiguous(N, MPI_INT, &vectorType);
+    // MPI_Type_commit(&vectorType);
+
 
     // here the chunk each processor needs will be scatter to it
-    MPI_Scatterv(matrix, chunkSize, displs, vectorType, chunk, chunkSize[rank], vectorType, 0, world);
- 
+    // MPI_Scatterv(&(graph[0][0]), chunkSize, displs, MPI_INT, chunk, chunkSize[rank], MPI_INT, MASTER, world);
 
-    MST = (int*)malloc(N*sizeof(int)); // max size is number of vertices
+    for (i = 0; i < N; ++i) {
+        chunk[i] = graph[rank][i];
+    }
+    
+
+    // for (i = 0; i < procNum; ++i)
+    //     chunkSize[i] /= N;
+
+ 
+    if (rank == 0) {
+        printf("rank %d \t chunk = [", rank);
+        for (i = 0; i < chunkSize[rank]*N; ++i)
+            printf("%d, ", chunk[i]);
+        printf("]\n");
+        printf("-------------------\n");
+    }
+
+
+    MST = calloc(N, sizeof(int));   // max size is number of vertices
 
     MST[0] = 0;
-    minWeight = 0;
+    for (i = 1; i < N; ++i)
+    {
+        MST[i] = -1;
+    }
+
+    globalMinWeight = 0;
     v1 = v2 = 0;
 
-
+    
     // iterates over vertices
     for (k = 0; k < N-1; ++k) 
     {
@@ -159,6 +212,9 @@ int main(int argc, char *argv[])
 
         localRow.minWeight = minWeight;
         localRow.rank = rank;
+
+        // printf("[DEBUG] rank=%d\t minWeight=%d\t v1=%d\t v2=%d\n", 
+            // rank, minWeight, v1, v2);
         
         // each process have to send its min weight row to others processes
         MPI_Allreduce(&localRow, &globalRow, 1, MPI_2INT, MPI_MINLOC, world); 
@@ -169,12 +225,13 @@ int main(int argc, char *argv[])
         MPI_Bcast(&edge, 1, MPI_2INT, globalRow.rank, world);
 
         MST[edge.v2] = edge.v1;
-        minWeight += globalRow.minWeight;
+        globalMinWeight += globalRow.minWeight;
     } // end of loop over vertices
 
+    MPI_Barrier(world);
 
     if (rank == MASTER) {
-        printf("MST weight: %d\n", minWeight);
+        printf("MST weight: %d\n", globalMinWeight);
         // dodać czas działania
         // zapisanie macierzy sąsiedztwa MST do pliku
         // printowanie krawędzi ?
@@ -182,13 +239,13 @@ int main(int argc, char *argv[])
 
 
     // dealocating memory
-    for(i = 0; i < N; ++i)
-        free(graph[i]);
-    free(graph);
-    free(chunkSize);
-    free(displ);
-    free(chunk);
-    free(MST);
+    // for(i = 0; i < N; ++i)
+    //     free(graph[i]);
+    // free(graph);
+    // free(chunkSize);
+    // free(displs);
+    // free(chunk);
+    // free(MST);
 
     MPI_Finalize();
 
